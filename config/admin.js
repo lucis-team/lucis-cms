b5f7b8e3a7c9ec54aa3eabb5d43e6012430f1ccd
WinePine <basitsaeed1@gmail.com>
2026-03-10 20:08:09 +0100

    [fix] Correct blog preview URL format in admin configuration

--- config/admin.js
+++ config/admin.js
@@ -6,7 +6,7 @@ const getPreviewPathname = (uid, { document, status, previewSecret }) => {
       return `/lp/${url}?status=${status}&secret=${previewSecret}`;
     }
     case "api::article.article": {
-      return `/blog/preview/${slug}?status=${status}&secret=${previewSecret}`;
+      return `/blog/${slug}/preview/?status=${status}&secret=${previewSecret}`;
     }
     default: {
       return null;
